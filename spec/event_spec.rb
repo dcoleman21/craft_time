b6fccf3681186e9fbdf33e2b36db1a2b84093d10
Dani Coleman <danicoleman00@gmail.com>
2023-04-12 07:58:40 -0600

    test: add attendee names, craft with most, supply list

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -16,4 +16,36 @@ RSpec.describe Event do
       expect(event.attendees).to eq([person])
     end
   end
+
+  describe "Iteration 2" do 
+    it "can get attendee names" do 
+      hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+      toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+      sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+      knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+      event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
+      
+      expect(event.attendee_names).to eq(["Hector", "Toni"])
+    end
+
+    it "can get teh craft with most supplies" do 
+      hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+      toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+      sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+      knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+      event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
+
+      expect(event.craft_with_most_supplies).to eq("sewing")
+    end
+
+    it "can get a supply list" do 
+      hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+      toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+      sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+      knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+      event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
+
+      expect(event.supply_list).to eq(["fabric", "scissors", "thread", "sewing_needles", "yarn", "knitting_needles"])
+    end
+  end
 end
